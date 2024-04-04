c277f118c0328094fa57320bfd3b420c101625d7
Edu <eduardperezmendez@gmail.com>
2024-04-04 19:56:11 +0200

    Modified the metaboAnalyst R style

--- Work/R/utils.R
+++ Work/R/utils.R
@@ -27,20 +27,32 @@ DE_to_metaboanalyst <- function(dataset_experiment) {
 # Load a dataframe to metaboanalystR data object
 # mSet <- InitDataObjects("conc", "stat", FALSE);
 # mSet <- Read.TextData(mSet, "")
+wd <- getwd()
+out_dir <- "analysis"
+plot_dir <- "plots"
+if (!dir.exists(out_dir)) {
+  dir.create(out_dir)
+}
 
-
-
-
+setwd(file.path(wd, out_dir))
+if (!dir.exists(plot_dir)) {
+  dir.create(plot_dir)
+}
 mSet <- InitDataObjects("conc", "stat", FALSE);
-mSet <- Read.TextData(mSet, "data/metaboData.csv")
+mSet <- Read.TextData(mSet, filePath = file.path(wd, "data/metaboData.csv"))
 mSet<-SanityCheckData(mSet);
 
-mSet<-ReplaceMin(mSet);
+# mSet<-ReplaceMin(mSet);
+mSet <- RemoveMissingPercent(mSet, percent=0.5) #Remove features containing a user-defined % cut-off of missing values
+# mSet <- ImputeMissingVar(mSet, method="exclude") #Remove variables with missing values
+mSet <- ImputeMissingVar(mSet, method="knn_smp") #Replace missing values with KNN imputed values   
+
+# Check if the sample size is too small, returns a 0 if the data passes the check
+mSet<-IsSmallSmplSize(mSet)
+
 mSet<-PreparePrenormData(mSet);
 mSet<-Normalization(mSet, "NULL", "LogNorm", "MeanCenter", "", ratio=FALSE, ratioNum=20);
-mSet<-PlotNormSummary(mSet, "norm_0_", format ="svg", dpi=72, width=NA);
-mSet<-PlotSampleNormSummary(mSet, "snorm_0_", format = "svg", dpi=72, width=NA)
+mSet<-PlotNormSummary(mSet, file.path(plot_dir, "norm_0_"), format ="svg", dpi=72, width=NA);
+mSet<-PlotSampleNormSummary(mSet, file.path(plot_dir, "snorm_0_"), format = "svg", dpi=72, width=NA)
 
 
-
-plot(mSet$snorm_0_)
